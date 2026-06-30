ac5e49f29a1f3497767684062ad8ab00c7dbd62b
Xqhare <xqhare@web.de>
2026-06-30 06:39:16 +0200

    more tests

--- tests/easy.rs
+++ tests/easy.rs
@@ -11,14 +11,42 @@ fn hello_world() {
 #[test]
 fn compare() {
     let text = format!(
-        "{}{}{}{}{}",
+        "{}{}{}{}{}{}",
         include_str!("../README.md"),
         include_str!("../LICENSE"),
         include_str!("../CONTRIBUTING.md"),
         include_str!("../Cargo.toml"),
         include_str!("../Cargo.lock"),
+        include_str!("../.gitignore")
     );
     let thot = Thoth::grapheme_segmentation(&text).unwrap();
     let unicode = text.graphemes(true).collect::<Vec<_>>();
     assert_eq!(thot, unicode);
+}
+
+/// Doesn't panic, but returns a vector of length 3 instead of the correct 1
+#[test]
+fn no_emoji() {
+    let emoji = "❤️";
+    let graphemes = Thoth::grapheme_segmentation(emoji).unwrap();
+    assert_ne!(graphemes.len(), 1);
+    assert_eq!(graphemes.len(), 3);
+}
+
+/// Panics with `attempt to subtract with overflow`
+#[test]
+#[should_panic]
+fn no_emoji2() {
+    let emoji = "🦀";
+    let graphemes = Thoth::grapheme_segmentation(emoji).unwrap();
+    assert_ne!(graphemes.len(), 1);
+}
+
+/// Panics with `attempt to subtract with overflow`
+#[test]
+#[should_panic]
+fn no_emoji3() {
+    let emoji = "✅";
+    let graphemes = Thoth::grapheme_segmentation(emoji).unwrap();
+    assert_ne!(graphemes.len(), 1);
 }
